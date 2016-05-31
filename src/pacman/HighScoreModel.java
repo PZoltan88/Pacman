901e322e9a9b5a702fc8012f6a7e24fe06569e35
604772006 <604772006@BTP076024.iuser.iroot.adidom.com>
2016-05-31 16:00:50 +0200

    Highscore model - file handling - serialize / deserialize to file

--- src/pacman/HighScoreModel.java
+++ src/pacman/HighScoreModel.java
@@ -5,7 +5,12 @@
  */
 package pacman;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
@@ -14,48 +19,87 @@ import java.util.ArrayList;
 import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
  * @author 604772006
  */
 public class HighScoreModel {
-    public static final String SCOREFILENAME="highscores.txt";
-    private static ArrayList<Score> hiScores;
-    
-        
-    public static void updateFile()
-    {
-        Charset utf8 = StandardCharsets.UTF_8;
-        List<String> lines = Arrays.asList("1st line", "2nd line");
+
+    public static final String SCOREFILENAME = "highscores.bin";
+    private ArrayList<Score> hiScores;
+
+    public void writeFile() {
+        ObjectOutputStream oos = null;
+        FileOutputStream fout = null;
         try {
-            Files.write(Paths.get(SCOREFILENAME), lines, utf8,
-                    StandardOpenOption.CREATE, StandardOpenOption.APPEND);
-        } catch (IOException ex) {
-            Logger.getLogger(HighScoreModel.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-    
-    public static void readFile()
-    {
-        Charset utf8 = StandardCharsets.UTF_8;
-        hiScores = new ArrayList<>();
-        try {
-            if(Files.notExists(Paths.get(SCOREFILENAME), LinkOption.NOFOLLOW_LINKS))
-            {
-                Files.createFile(Paths.get(SCOREFILENAME));
+            fout = new FileOutputStream(SCOREFILENAME, true);
+            oos = new ObjectOutputStream(fout);
+            oos.writeObject(hiScores);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            if (oos != null) {
+                try {
+                    oos.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(HighScoreModel.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
-        for ( String s :Files.readAllLines(Paths.get(SCOREFILENAME), utf8))  
-        {
-            System.out.println(s);
-        }
-        
-        } catch (IOException ex) {
-            Logger.getLogger(HighScoreModel.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    private class Score{
+
+    public void updateHiScore(String name, int score) {
+        hiScores.add(new Score(0, name, score));
+        writeFile();
+    }
+
+    public void readFile() {
+        /*
+         Charset utf8 = StandardCharsets.UTF_8;
+         hiScores = new ArrayList<>();
+         try {
+         if(Files.notExists(Paths.get(SCOREFILENAME), LinkOption.NOFOLLOW_LINKS))
+         {
+         Files.createFile(Paths.get(SCOREFILENAME));
+         }
+         for ( String s :Files.readAllLines(Paths.get(SCOREFILENAME), utf8))  
+         {
+         System.out.println(s);
+         }
+        
+         } catch (IOException ex) {
+         Logger.getLogger(HighScoreModel.class.getName()).log(Level.SEVERE, null, ex);
+         }
+         */
+        hiScores = new ArrayList<>();
+        ObjectInputStream objectinputstream = null;
+        try {
+            FileInputStream streamIn = new FileInputStream(SCOREFILENAME);
+            objectinputstream = new ObjectInputStream(streamIn);
+            ArrayList<Score> readCase = (ArrayList<Score>) objectinputstream.readObject();
+            hiScores = readCase;
+            //System.out.println(recordList.get(i));
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (objectinputstream != null) {
+                try {
+                    objectinputstream.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(HighScoreModel.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+    }
+
+    public DefaultTableModel getHiScoreData() {
+        return null;
+    }
+
+    private class Score implements Serializable {
+
         private int rank;
         private String playerName;
         private int score;
@@ -73,7 +117,11 @@ public class HighScoreModel {
         public void setRank(int rank) {
             this.rank = rank;
         }
-        
-        
+
+        @Override
+        public String toString() {
+            return "Score{" + "rank=" + rank + ", playerName=" + playerName + ", score=" + score + '}';
+        }
+
     }
 }
